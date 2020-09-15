7d5a550eeb345aed49b2d87d8b19a16238b5dd92
Nguyen Tuan An <nguyentuanan.iuh@gmail.com>
2020-09-16 00:41:03 +0700

    Add lib main express-main-layout main layout

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const express = require('express');
+var path = require('path')
 const app = express()
+var morgan = require('morgan')
+var expresLayout = require("express-ejs-layouts")
 const port = 3000
 
-app.get('/newss', (req, res) => {return res.send("Dựng server thành công")} )
+//log Http Request
+app.use(morgan('combined'))
+//expres layout
+app.use(expresLayout)
+//Static file
+app.use(express.static(path.join(__dirname,'src/public/')))
+console.log('haha'+ path.join(__dirname,'src/public'))
+
+//view Engine
+app.set("view engine", "ejs");
+app.set("views","./src/resource/views/")
+//static file
+
+
+app.get('/', (req, res) => {
+    res.render("layout.ejs")
+} )
+
+app.get('/Home', (req, res) => {
+    res.render("layout/home.ejs")
+} )
+
+app.get('/Test', (req, res) => {
+    res.render("layout/test.ejs")
+} )
+
+
 
 
 app.listen(port, () => console.log("Đang lắng nghe port 3000, thanks you!"))
