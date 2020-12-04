59c9b54ed91647c03bda886d61c97bc52b995dd3
Nguyen Tuan An <nguyentuanan.iuh@gmail.com>
2020-12-04 22:15:43 +0700

    .

--- database_sql.js
+++ database_sql.js
@@ -2,29 +2,19 @@
 const Sequelize = require('sequelize');
 //connect database sql
 
-<<<<<<< HEAD
 
 
 module.exports = new Sequelize('Recuirement', 'corbinnguyen', '123@Tuanan1996', {
     host: 'recuirement.database.windows.net',
-=======
-module.exports = new Sequelize('Recuirement', 'sa', 'corbinnguyen1996', {
-    host: '192.168.1.5',
->>>>>>> 0c89f12f1dff22779ee60dac9587728ac3bb5e7e
     dialect: 'mssql',
     port:1433,
     dialectOptions: {
         encrypt: true,
-<<<<<<< HEAD
         useUTC: false, //for reading from database
         dateStrings: true,
         typeCast: true,
         instanceName: 'recuirement.database.windows.net'
     },
     timezone: '+07:00'
-=======
-        instanceName: 'MSSQLSERVER124234324'
-    }
->>>>>>> 0c89f12f1dff22779ee60dac9587728ac3bb5e7e
 });
 
